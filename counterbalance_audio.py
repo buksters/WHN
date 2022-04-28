6bc9e9f9adda67f620e3c66d9f6b4036953305af
buksters <ibukiwasaki@gmail.com>
2022-04-28 00:56:39 -0400

    updated counterbalance_audio

--- counterbalance_audio.py
+++ counterbalance_audio.py
@@ -10,28 +10,37 @@ import os
 from pydub import AudioSegment
 from pydub.playback import play
 
+specific = ['tablet', 'outside', 'jacket', 'tub']
+broad = ['computer', 'room', 'jump', 'm&ms']
+scalar = ['writing', 'popcorn', 'peas', 'laundry']
+stories = specific + broad + scalar
+
 sound_dir = '/Users/ibuki/Documents/GitHub/WHN/mp3' 
 os.chdir(sound_dir)
 
-intro = AudioSegment.from_file("tablet_options_intro.mp3", format="mp3")
-compliance = AudioSegment.from_file("tablet_compliance.mp3", format="mp4")
-loophole = AudioSegment.from_file("tablet_loophole.mp3", format="mp4")
-noncompliance = AudioSegment.from_file("tablet_noncompliance.mp3", format="mp4")
-or_audio = AudioSegment.from_file("or.mp3", format="mp4")
-combined = intro + compliance + loophole + or_audio + noncompliance
+def create_perms(stories):
+    for story in stories:
+        intro = AudioSegment.from_file("{story}/options_intro.mp3", format="mp3")
+        compliance = AudioSegment.from_file("{story}/compliance.mp3", format="mp4")
+        loophole = AudioSegment.from_file("{story}/loophole.mp3", format="mp4")
+        noncompliance = AudioSegment.from_file("{story}/noncompliance.mp3", format="mp4")
+        or_audio = AudioSegment.from_file("or.mp3", format="mp4")
+        combined = intro + compliance + loophole + or_audio[700:-500] + noncompliance
+        
+        # play(combined)
+        
+        file_handle = combined.export("{story}/options_combined.mp3", format="mp3")
+        
+        start_first = round(intro.duration_seconds, 2)
+        start_second = round(intro.duration_seconds + compliance.duration_seconds, 2)
+        end_second = round(intro.duration_seconds + compliance.duration_seconds + loophole.duration_seconds, 2)
+        start_third = round(intro.duration_seconds + compliance.duration_seconds + loophole.duration_seconds + or_audio[700:-500].duration_seconds, 2)
 
-# play(combined)
+        print(f'''highlights for {story} should be at: 
+              first: {start_first} to {start_second}
+              second: {start_second} to {end_second}
+              third: {start_third} to {round(combined.duration_seconds, 2)}''')
 
-# simple export
-file_handle = combined.export("tablet_options_combined.mp3", format="mp3")
 
-start_first = round(intro.duration_seconds, 2)
-start_second = round(intro.duration_seconds + compliance.duration_seconds, 2)
-end_second = round(intro.duration_seconds + compliance.duration_seconds + loophole.duration_seconds, 2)
-start_third = round(intro.duration_seconds + compliance.duration_seconds + loophole.duration_seconds + or_audio[700:-500].duration_seconds, 2)
 
-print(f'''highlights should be at: 
-      first: {start_first} to {start_second}
-      second: {start_second} to {end_second}
-      third: {start_third} to {round(combined.duration_seconds, 2)}''')
       
